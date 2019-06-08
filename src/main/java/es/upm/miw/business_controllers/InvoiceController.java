dac98f357b65ea2586401ca105cb93cbf4b0d386
Dennys <dennys.landy@hotmail.com>
2019-06-08 06:31:38 +0200

    add InvoiceController #6, edit InvoiceDto, InvoiceRepository #6

---
+++ src/main/java/es/upm/miw/business_controllers/InvoiceController.java
@@ -0,0 +1,33 @@
+package es.upm.miw.business_controllers;
+
+import es.upm.miw.documents.InvoiceLine;
+import es.upm.miw.dtos.InvoiceDto;
+import es.upm.miw.repositories.InvoiceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class InvoiceController {
+
+    @Autowired
+    private InvoiceRepository invoiceRepository;
+
+    public List<InvoiceDto> readAll() {
+        List<InvoiceDto> invoiceDtos=  new ArrayList<>();
+        for (InvoiceDto dto : invoiceRepository.findAllInvoices()) {
+            for (InvoiceLine invoiceLine : dto.getInvoiceLines()) {
+                InvoiceDto invoiceDto ;
+                invoiceDto=new InvoiceDto(dto.getCreationDate(),dto.getDescription(),dto.getInvoiceLines());
+                invoiceDtos.add(invoiceDto);
+                System.out.println("invoiceDto: " + invoiceDto);
+                System.out.println("Invoice line: " + invoiceLine);
+            }
+        }
+        System.out.println(",,,,,,,,,,;"+ invoiceDtos);
+        return invoiceDtos ;
+    }
+}
--- src/main/java/es/upm/miw/repositories/InvoiceRepository.java
+++ src/main/java/es/upm/miw/repositories/InvoiceRepository.java
@@ -2,12 +2,13 @@ package es.upm.miw.repositories;
 
 import es.upm.miw.documents.Invoice;
 import es.upm.miw.dtos.InvoiceDto;
+import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
 
-public class InvoiceRepository {
+public interface InvoiceRepository extends MongoRepository<Invoice, String> {
 
-    //@Query(value = "{}", fields = "{ 'id' : 0, 'description' : 1, 'creationDate' : 1, 'invoiceLines': 1}")
-    //List<InvoiceDto> findAllOrders();
+    @Query(value = "{}", fields = "{'id' : 0, 'description' : 1, 'creationDate' : 1, 'invoiceLines': 1}")
+    List<InvoiceDto> findAllInvoices();
 }
