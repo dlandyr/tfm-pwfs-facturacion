27c81b98be390f29cb1e7e6938cec7eb2dcaa689
Dennys <dennys.landy@hotmail.com>
2019-06-22 17:22:57 +0200

    add new exceptios #12

--- src/main/java/es/upm/miw/rest_controllers/InvoiceResource.java
+++ src/main/java/es/upm/miw/rest_controllers/InvoiceResource.java
@@ -1,11 +1,14 @@
 package es.upm.miw.rest_controllers;
 
 import es.upm.miw.business_controllers.InvoiceController;
+import es.upm.miw.dtos.InvoiceDto;
 import es.upm.miw.dtos.InvoiceSearchDto;
+import org.apache.logging.log4j.LogManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 
 //@PreAuthorize("hasRole('ADMIN') or hasRole('MANAGER') or hasRole('OPERATOR')")
@@ -22,5 +25,10 @@ public class InvoiceResource {
     @GetMapping
     public List<InvoiceSearchDto> readAll() {
         return this.invoiceController.readAll();
+    }
+
+    @PostMapping
+    public InvoiceDto createInvoice(@Valid @RequestBody InvoiceDto invoice) {
+        return this.invoiceController.create(invoice);
     }
 }
