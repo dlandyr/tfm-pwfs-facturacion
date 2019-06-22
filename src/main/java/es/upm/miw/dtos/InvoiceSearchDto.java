c332974932f328826514ea69a660c1753cfda02d
Dennys <dennys.landy@hotmail.com>
2019-06-22 05:07:31 +0200

    edit Invoices files #11

--- src/main/java/es/upm/miw/dtos/InvoiceSearchDto.java
+++ src/main/java/es/upm/miw/dtos/InvoiceSearchDto.java
@@ -1,24 +1,67 @@
 package es.upm.miw.dtos;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
 import java.math.BigDecimal;
 
+@JsonInclude(Include.NON_NULL)
 public class InvoiceSearchDto {
 
+    private String descriptionUser;
+    private String descriptionCustomer;
+    private String descriptionEnterprise;
     private String descriptionInvoice;
     private String descriptionProduct;
     private Integer quantity;
     private BigDecimal unitPrice;
     private BigDecimal calculatedTax;
+    private BigDecimal tax;
+    private BigDecimal subtotal;
+    private BigDecimal discount;
+    private BigDecimal total;
 
     public InvoiceSearchDto(){
     }
 
-    public InvoiceSearchDto(String descriptionInvoice, String descriptionProduct, Integer quantity, BigDecimal unitPrice, BigDecimal calculatedTax){
+    public InvoiceSearchDto(String descriptionUser, String descriptionCustomer, String descriptionEnterprise, String descriptionInvoice, String descriptionProduct,
+                            Integer quantity, BigDecimal unitPrice, BigDecimal calculatedTax, BigDecimal tax, BigDecimal subtotal, BigDecimal discount, BigDecimal total){
+        this.descriptionUser = descriptionUser;
+        this.descriptionCustomer = descriptionCustomer;
+        this.descriptionEnterprise = descriptionEnterprise;
         this.descriptionInvoice = descriptionInvoice;
         this.descriptionProduct = descriptionProduct;
         this.quantity = quantity;
         this.unitPrice = unitPrice;
         this.calculatedTax = calculatedTax;
+        this.tax = tax;
+        this.subtotal = subtotal;
+        this.discount = discount;
+        this.total = total;
+    }
+
+    public String getDescriptionUser() {
+        return descriptionUser;
+    }
+
+    public void setDescriptionUser(String descriptionUser) {
+        this.descriptionUser = descriptionUser;
+    }
+
+    public String getDescriptionCustomer() {
+        return descriptionCustomer;
+    }
+
+    public void setDescriptionCustomer(String descriptionCustomer) {
+        this.descriptionCustomer = descriptionCustomer;
+    }
+
+    public String getDescriptionEnterprise() {
+        return descriptionEnterprise;
+    }
+
+    public void setDescriptionEnterprise(String descriptionEnterprise) {
+        this.descriptionEnterprise = descriptionEnterprise;
     }
 
     public String getDescriptionInvoice() {
@@ -61,14 +104,53 @@ public class InvoiceSearchDto {
         this.calculatedTax = calculatedTax;
     }
 
+    public BigDecimal getTax() {
+        return tax;
+    }
+
+    public void setTax(BigDecimal tax) {
+        this.tax = tax;
+    }
+
+    public BigDecimal getSubtotal() {
+        return subtotal;
+    }
+
+    public void setSubtotal(BigDecimal subtotal) {
+        this.subtotal = subtotal;
+    }
+
+    public BigDecimal getDiscount() {
+        return discount;
+    }
+
+    public void setDiscount(BigDecimal discount) {
+        this.discount = discount;
+    }
+
+    public BigDecimal getTotal() {
+        return total;
+    }
+
+    public void setTotal(BigDecimal total) {
+        this.total = total;
+    }
+
     @Override
     public String toString() {
         return "InvoiceSearchDto{" +
-                "descriptionInvoice='" + descriptionInvoice + '\'' +
+                "descriptionUser='" + descriptionUser + '\'' +
+                ", descriptionCustomer='" + descriptionCustomer + '\'' +
+                ", descriptionEnterprise='" + descriptionEnterprise + '\'' +
+                ", descriptionInvoice='" + descriptionInvoice + '\'' +
                 ", descriptionProduct='" + descriptionProduct + '\'' +
                 ", quantity=" + quantity +
                 ", unitPrice=" + unitPrice +
                 ", calculatedTax=" + calculatedTax +
+                ", tax=" + tax +
+                ", subtotal=" + subtotal +
+                ", discount=" + discount +
+                ", total=" + total +
                 '}';
     }
 }
